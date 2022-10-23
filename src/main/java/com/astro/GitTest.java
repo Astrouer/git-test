d08f0a5288cdd8c2932d6a4854d99af2b35c1966
Astro <191479291@qq.com>
2022-10-23 23:06:59 +0800

    这是首次提交到本地库测试

---
+++ src/main/java/com/astro/GitTest.java
@@ -0,0 +1,13 @@
+package com.astro;
+
+/**
+ * @author Astro
+ * @version 1.0
+ * @description:
+ * @date 2022/10/23 22:55
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("这是第一行测试代码 请检查并提交金宝,谢谢");
+    }
+}
