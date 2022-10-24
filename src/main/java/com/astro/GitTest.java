b470bf473b5d73bd3c339019c8ec3a456db77986
Astro <191479291@qq.com>
2022-10-24 19:54:48 +0800

    推送测试

--- src/main/java/com/astro/GitTest.java
+++ src/main/java/com/astro/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("这行测试代码由master修改 修改 请检查并提交金宝,谢谢");
         System.out.println("hot-fix 修改 可能与master修改的产生冲突 请检查并提交金宝,谢谢");
         System.out.println("首次push测试 请检查并提交金宝,谢谢");
+        System.out.println("首次码云Gitee测试 push测试 请检查并提交金宝,谢谢");
     }
 }
